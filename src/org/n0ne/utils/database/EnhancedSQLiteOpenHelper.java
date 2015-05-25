47703c53359d91a5f3368b351f79e91f5e6c62bf
n0ne <new.new.none@gmail.com>
2015-05-25 12:13:40 -0400

    Added a beta version of EnhancedSQLiteOpenHelper.

---
+++ src/org/n0ne/utils/database/EnhancedSQLiteOpenHelper.java
@@ -0,0 +1,56 @@
+package org.n0ne.utils.database;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public abstract class EnhancedSQLiteOpenHelper extends SQLiteOpenHelper {
+
+    private int mDatabaseRawId;
+
+    protected final Context mContext;
+
+    private static final Pattern sCommandSeparatorPattern = Pattern.compile("[^;]+?;\\s*(?:;*?END;)?");
+
+    public EnhancedSQLiteOpenHelper (Context context, String name, int version, int databaseRawId) {
+        this(context.getApplicationContext(), name, version, databaseRawId, null);
+    }
+
+    public EnhancedSQLiteOpenHelper (Context context, String name, int version,
+                                     int databaseRawId, SQLiteDatabase.CursorFactory factory) {
+        super(context, name, factory, version);
+        mContext = context;
+        mDatabaseRawId = databaseRawId;
+    }
+
+    @Override
+    public void onCreate (SQLiteDatabase db) {
+        final Scanner reader;
+        String  command;
+
+        try {
+            reader = new Scanner(mContext.getResources().openRawResource(mDatabaseRawId));
+        } catch (Resources.NotFoundException e) {
+            throw new IllegalStateException(
+                    "You should declare an .sql file under /res/raw, containing SQL" +
+                            "code for initializing your database."
+            );
+        }
+
+        while ((command = reader.findWithinHorizon(sCommandSeparatorPattern, 0)) != null) {
+            db.execSQL(command);
+        }
+
+        reader.close();
+    }
+
+    @Override
+    public void onUpgrade (SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+}
